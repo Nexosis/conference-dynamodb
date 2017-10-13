268f492ddd999aa7151fa795bd68b80d18d64474
John Bledsoe <jbledsoe@nexosis.com>
2017-10-12 22:32:35 -0400

    Added create, drop, and scale operations

--- Nexosis.Conference.DynamoDB/DynamoBuddy.cs
+++ Nexosis.Conference.DynamoDB/DynamoBuddy.cs
@@ -1,11 +1,79 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
 
 namespace Nexosis.Conference.DynamoDB
 {
     public class DynamoBuddy
     {
+        private static readonly AWSCredentials credentials = new EnvironmentVariablesAWSCredentials();
+        private static readonly AmazonDynamoDBConfig config = new AmazonDynamoDBConfig
+        {
+            RegionEndpoint = RegionEndpoint.USEast2,
+        };
+
+        private readonly TextWriter output;
+
+        public DynamoBuddy(TextWriter output)
+        {
+            this.output = output;
+        }
+
         public async Task<int> CreateTables()
         {
+            using (var client = new AmazonDynamoDBClient(credentials, config))
+            {
+                await Task.WhenAll(
+                    CreateTable(client, "nexosis-conference-data", "series-key", "date"),
+                    CreateTable(client, "nexosis-conference-grouped-data", "series-key", "start-date"));
+            }
+
+            return 0;
+        }
+
+
+        public async Task<int> DropTables()
+        {
+            using (var client = new AmazonDynamoDBClient(credentials, config))
+            {
+                await Task.WhenAll(
+                    DropTable(client, "nexosis-conference-data"),
+                    DropTable(client, "nexosis-conference-grouped-data"));
+            }
+
+            return 0;
+        }
+
+        public async Task<int> ScaleTable(string tableName, int reads, int writes)
+        {
+            using (var client = new AmazonDynamoDBClient(credentials, config))
+            {
+                var request = new UpdateTableRequest
+                {
+                    TableName = tableName,
+                    ProvisionedThroughput = new ProvisionedThroughput
+                    {
+                        ReadCapacityUnits = reads,
+                        WriteCapacityUnits = writes
+                    }
+                };
+
+                try
+                {
+                    await output.WriteLineAsync($"Scaling table: {tableName}");
+                    await client.UpdateTableAsync(request);
+                    await output.WriteLineAsync($"Scaled table: {tableName}");
+                }
+                catch (AmazonDynamoDBException ex)
+                {
+                    await output.WriteLineAsync(ex.Message);
+                }
+            }
+
             return 0;
         }
 
@@ -17,6 +85,55 @@ namespace Nexosis.Conference.DynamoDB
         public async Task<int> ReadData(int? datasetCount, int? iterationCount, bool runContinuous, bool runParallel, bool usePages)
         {
             return 0;
+        }
+
+        private async Task CreateTable(AmazonDynamoDBClient client, string tableName, string hashKeyName, string rangeKeyName)
+        {
+            var request = new CreateTableRequest
+            {
+                TableName = tableName,
+                KeySchema = new List<KeySchemaElement>
+                {
+                    new KeySchemaElement { AttributeName = hashKeyName, KeyType = KeyType.HASH },
+                    new KeySchemaElement { AttributeName = rangeKeyName, KeyType = KeyType.RANGE },
+                },
+                AttributeDefinitions = new List<AttributeDefinition>
+                {
+                    new AttributeDefinition { AttributeName = hashKeyName, AttributeType = ScalarAttributeType.S },
+                    new AttributeDefinition { AttributeName = rangeKeyName, AttributeType = ScalarAttributeType.S },
+                },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 10,
+                    WriteCapacityUnits = 10
+                },
+            };
+
+            try
+            {
+                await output.WriteLineAsync($"Creating table: {tableName}");
+                await client.CreateTableAsync(request);
+                await output.WriteLineAsync($"Created table: {tableName}");
+            }
+            catch (AmazonDynamoDBException ex)
+            {
+                await output.WriteLineAsync(ex.Message);
+            }
+        }
+
+        private async Task DropTable(AmazonDynamoDBClient client, string tableName)
+        {
+            try
+            {
+                await output.WriteLineAsync($"Dropping table: {tableName}");
+                await client.DeleteTableAsync(tableName);
+                await output.WriteLineAsync($"Dropped table: {tableName}");
+
+            }
+            catch (AmazonDynamoDBException ex)
+            {
+                await output.WriteLineAsync(ex.Message);
+            }
         }
     }
 }
--- Nexosis.Conference.DynamoDB/Program.cs
+++ Nexosis.Conference.DynamoDB/Program.cs
@@ -6,12 +6,14 @@ namespace Nexosis.Conference.DynamoDB
     {
         static readonly CommandLineApplication application = new CommandLineApplication();
 
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             try
             {
-                var dynamoBuddy = new DynamoBuddy();
+                var dynamoBuddy = new DynamoBuddy(application.Out);
 
+                application.Name = "Nexosis Conference DynamoDB Buddy";
+                application.Description = "Demonstrates various problems and remedies when interacting with Amazon DynamoDB";
                 application.HelpOption("-?|-h|--help");
 
                 application.Command("create", command =>
@@ -22,6 +24,29 @@ namespace Nexosis.Conference.DynamoDB
                     command.OnExecute(() => dynamoBuddy.CreateTables());
                 });
 
+                application.Command("drop", command =>
+                {
+                    command.Description = "Drops DynamoDB tables created for the application";
+                    command.HelpOption("-?|-h|--help");
+
+                    command.OnExecute(() => dynamoBuddy.DropTables());
+                });
+
+                application.Command("scale", command =>
+                {
+                    command.Description = "Scales DynamoDB tables to the specified values";
+                    command.HelpOption("-?|-h|--help");
+
+                    var table = command.Option("-t|--table", "name of the table to scale", CommandOptionType.SingleValue);
+                    var reads = command.Option("-r|--reads", "read capacity units to scale to", CommandOptionType.SingleValue);
+                    var writes = command.Option("-w|--writes", "write capacity units to scale to", CommandOptionType.SingleValue);
+
+                    command.OnExecute(() => dynamoBuddy.ScaleTable(
+                        table.Value(),
+                        GetInt32Value(reads, true).Value,
+                        GetInt32Value(writes, true).Value));
+                });
+
                 application.Command("write", command =>
                 {
                     command.Description = "Writes to DynamoDB tables";
@@ -30,7 +55,7 @@ namespace Nexosis.Conference.DynamoDB
                     var datasetCount = command.Option("-d|--datasets", "count of datasets to write", CommandOptionType.SingleValue);
                     var rowCount = command.Option("-r|--rows", "count of rows per dataset to write", CommandOptionType.SingleValue);
                     var iterationCount = command.Option("-i|--iterations <count>", "count of iterations to execute", CommandOptionType.SingleValue);
-                    var runContinuous = command.Option("-c|--continuous", "run continuously until a key is pressed", CommandOptionType.NoValue);
+                    var runContinuous = command.Option("-c|--continuous", "run continuously until the application is shut down", CommandOptionType.NoValue);
                     var runParallel = command.Option("-p|--parallel", "write each dataset in parallel", CommandOptionType.NoValue);
                     var usePages = command.Option("-g|--group", "group data into pages", CommandOptionType.NoValue);
 
@@ -51,7 +76,7 @@ namespace Nexosis.Conference.DynamoDB
 
                     var datasetCount = command.Option("-d|--datasets", "count of datasets to read", CommandOptionType.SingleValue);
                     var iterationCount = command.Option("-i|--iterations <count>", "count of iterations to execute", CommandOptionType.SingleValue);
-                    var runContinuous = command.Option("-c|--continuous", "run continuously until a key is pressed", CommandOptionType.NoValue);
+                    var runContinuous = command.Option("-c|--continuous", "run continuously until the application is shut down", CommandOptionType.NoValue);
                     var runParallel = command.Option("-p|--parallel", "read each dataset in parallel", CommandOptionType.NoValue);
                     var usePages = command.Option("-g|--group", "group data into pages", CommandOptionType.NoValue);
 
@@ -64,19 +89,18 @@ namespace Nexosis.Conference.DynamoDB
                     ));
                 });
 
-
-                application.Execute(args);
+                return application.Execute(args);
             }
             catch (CommandParsingException cpe)
             {
-                var errorConsole = AnsiConsole.GetError(false);
+                application.Error.WriteLine("");
+                application.Error.WriteLine("Error: " + cpe.Message);
 
-                errorConsole.WriteLine("");
-                errorConsole.WriteLine("Error: " + cpe.Message);
+                return -1;
             }
         }
 
-        private static int? GetInt32Value(CommandOption option)
+        private static int? GetInt32Value(CommandOption option, bool require = true)
         {
             if (option.HasValue())
             {
@@ -91,7 +115,10 @@ namespace Nexosis.Conference.DynamoDB
             }
 
             // Option isn't specified
-            return null;
+            if (require)
+                throw new CommandParsingException(application, $"Option '{option.Template}' must be supplied with an integer value");
+            else
+                return null;
         }
     }
 }
